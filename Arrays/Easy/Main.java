ad6cc602673fb0a33e8ac6482468782ac8ccc484
Apoorv Jain <apoorvjainji@gmail.com>
2026-01-11 20:08:16 +0530

    New Questions Added(Arrays)

--- Arrays/Easy/Main.java
+++ Arrays/Easy/Main.java
@@ -8,7 +8,10 @@ public class Main {
             arr[i] = scanner.nextInt();
         }
         System.out.println(removeDuplicates(arr));
-        
+        System.out.println(check(arr));
+        int[] arr1 = {2, 2, 3, 4, 5};
+        int[] arr2 ={1, 1, 2, 3, 4};
+
     }
     // maximum element of the array
     public static int maximumElement(int[] arr) {
@@ -19,6 +22,16 @@ public class Main {
             }
         }
         return max;
+    }
+    // missing number in array
+    public static int missingNumber(int[] nums) {
+        int n=nums.length;
+        int totalSum=n*(n+1)/2;
+        int sum=0;
+        for(int num:nums){
+            sum+=num;
+        }
+        return totalSum - sum;
     }
     // second maximum element of the array
     public static int secondMax(int[] arr){
@@ -34,7 +47,6 @@ public class Main {
         }       
         return secondMax;
     }
-
     // LeetCode Problem 26: Remove Duplicates from Sorted Array
     public static int removeDuplicates(int[] nums) {
         if (nums.length == 0) return 0;
@@ -48,6 +60,7 @@ public class Main {
         }
         return left + 1;
     }
+    // check if the array is sorted and rotated
     public static boolean check(int[] nums) {
         int count=0;
         for(int i=0; i<nums.length; i++){
@@ -58,4 +71,39 @@ public class Main {
         }
         return true;
     }
+    // move zeroes to the end of the array
+     public static void moveZeroes(int[] nums) {
+        int i = 0;
+        int j=0;
+        while(j<nums.length){
+        if(nums[j]!=0){
+            int temp = nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+        }
+    }
+        for(int num:nums){
+            System.out.print(num+" ");
+        }
+    }
+    // single number
+    public static int singleNumber(int[] nums) {
+        int answer=0;
+        for(int num:nums){
+            answer^=num;
+        }
+        return answer;
+    }
+    // residue prefixes Leetcode 3803
+     public static int residuePrefixes(String s) {
+        Set<Character> f = new HashSet<>();
+        int ans = 0;
+        for(int i = 0; i < s.length(); i++) {
+            f.add(s.charAt(i));
+            if(f.size() == (i + 1) % 3) ans++;
+        }
+        return ans;
+    }
+   
 }
--- Arrays/Medium/Solution.java
+++ Arrays/Medium/Solution.java
@@ -2,10 +2,13 @@ import java.util.*;
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+        // int n = scanner.nextInt();
         int[] arr = {0,1,0,3,12};
         int k = scanner.nextInt();
         rotate(arr, k);
+        int[] arr2 = {4,1,2,1,2};
+        System.out.println(findUnion(arr, arr2));
+        scanner.close();
     }
     // rotate the array by k steps 
     public static void rotate(int[] nums, int k) {
@@ -25,16 +28,47 @@ public class Solution {
             end--;
         }
     }
-    // move zeroes
-    public static void moveZeroes(int[] nums) {
-        int i = 0;
-        int j=0;
-        while(j<nums.length){
-        if(nums[j]!=0){
-            int temp = nums[i];
-            nums[i]=nums[j];
-            nums[j]=temp;
+     // union of two sorted arrays
+    public static ArrayList<Integer> findUnion(int a[], int b[]) {
+        ArrayList<Integer> result = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        int i=0, j=0;
+        while(i<a.length && j<b.length){
+            if(a[i]<b[j]){
+                if(!set.contains(a[i])){
+                    result.add(a[i]);
+                    set.add(a[i]);
+                }
+                i++;
+            } else if(b[j]<a[i]){
+                if(!set.contains(b[j])){
+                    result.add(b[j]);
+                    set.add(b[j]);
+                }
+                j++;
+            } else {
+                if(!set.contains(a[i])){
+                    result.add(a[i]);
+                    set.add(a[i]);
+                }
+                i++;
+                j++;
+            }
+        }
+        while(i<a.length){
+            if(!set.contains(a[i])){
+                result.add(a[i]);
+                set.add(a[i]);
+            }
             i++;
         }
+        while(j<b.length){
+            if(!set.contains(b[j])){
+                result.add(b[j]);
+                set.add(b[j]);
+            }
+            j++;
+        }
+        return result;
     }
 }
