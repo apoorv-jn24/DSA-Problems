02ecaeed21cc435d41d6a688aa0c7bd7024beed7
Apoorv Jain <apoorvjainji@gmail.com>
2026-01-27 20:40:54 +0530

    Binary search

--- Searching/Binary Search/Main.java
+++ Searching/Binary Search/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         
@@ -16,5 +18,54 @@ public class Main {
             }
         }
         return -1;
+    }
+    // First And last occurence of element in sorted array
+    public static int[] searchRange(int[] nums, int target) {
+        int[] arr = {-1, -1};
+        int idx=-1;
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                idx=mid;
+                high=mid-1; 
+            }
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        arr[0]=idx;
+        low = 0; high = nums.length - 1; idx=-1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                idx=mid;
+                low=mid+1; 
+            }
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        arr[1]=idx;
+        return arr;
+    }
+    public  static int mySqrt(int x) {
+        if (x == 0) return 0;
+        int low = 1, high = x;
+        int result = 0;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (mid <= x / mid) {
+                result = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return result;
     }
 }
