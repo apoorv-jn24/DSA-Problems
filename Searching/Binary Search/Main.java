2bfd0962cb1dcba3abaef759f170e336037ad80f
Apoorv Jain <apoorvjainji@gmail.com>
2026-01-28 20:36:36 +0530

    Updated problems of binary search

--- Searching/Binary Search/Main.java
+++ Searching/Binary Search/Main.java
@@ -1,11 +1,11 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         
     }
     // binary search general code
-    public static int search(int[] nums, int target) {
+    public static int binarySearch(int[] nums, int target) {
         int low = 0, high = nums.length - 1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
@@ -53,6 +53,7 @@ public class Main {
         arr[1]=idx;
         return arr;
     }
+    // square root of a number
     public  static int mySqrt(int x) {
         if (x == 0) return 0;
         int low = 1, high = x;
@@ -67,5 +68,60 @@ public class Main {
             }
         }
         return result;
+    }
+    // find peak element
+    public static int findPeakElement(int[] nums) {
+        if(nums.length==1) return 0;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[nums.length-1]>nums[nums.length-2]) return nums.length-1;
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > nums[mid + 1] && nums[mid] > nums[mid - 1]) {
+                return mid;
+            } else if (nums[mid] < nums[mid + 1]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+}
+// search in rotated sorted array
+    public static int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[low] <= nums[mid]) {
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+    // find minimum in rotated sorted array
+    public static int findMin(int[] nums) {
+        int low = 0, high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return nums[low];
     }
 }
