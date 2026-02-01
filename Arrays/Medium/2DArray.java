59701f82d69b72db702ff7c3c736d74911a2a011
Apoorv Jain <apoorvjainji@gmail.com>
2026-02-01 16:45:58 +0530

    Search a matrix

---
+++ Arrays/Medium/2DArray.java
@@ -0,0 +1,17 @@
+public class 2DArray {
+    public static void main(String[] args) {
+        
+    }
+    // Search a 2D matrix-II
+    public  static boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length, cols = matrix[0].length;
+        int i=0, j=cols-1;
+        while(i<rows && j>=0){
+            if(matrix[i][j] == target) return true;
+            else if(matrix[i][j] > target) j--;
+            else i++;
+        }
+        return false;
+    }
+
+}
