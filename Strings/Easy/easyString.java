74aa3b54d28d5941b898f7d4b257d2325f16cf31
Apoorv Jain <apoorvjainji@gmail.com>
2026-02-06 16:52:15 +0530

    Strings Today

---
+++ Strings/Easy/easyString.java
@@ -0,0 +1,61 @@
+import java.util.*;
+public class easyString {
+    public static void main(String[] args) {
+        
+    }
+    // Remove outermost parentheses - LeetCode 1021
+    public static String removeOuterParentheses(String s) {
+        StringBuilder sb = new StringBuilder();
+        int count=0;
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch=='('){
+                if(count>0){
+                    sb.append(ch);
+                }
+                count++;
+            } else {
+                count--;
+                if(count>0){
+                    sb.append(ch);
+                }
+            }
+        }
+        return sb.toString();
+    }
+    // largest odd number in the string
+    public static String largestOddNumber(String s) {
+        for(int i=s.length()-1; i>=0; i--){
+            if((s.charAt(i)-'0')%2==1) return s.substring(0,i+1);
+        }
+        return "";
+    }
+    // rotate string - leetcode 796
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) return false;
+        String target= s+s;
+        return target.contains(goal);
+    }
+    // isomorphic strings- leetcode 205
+    public static boolean isIsomorphic(String s, String t) {
+        if(s.length() != t.length()) return false;
+        char[] mapS = new char[256];
+        char[] mapT = new char[256];
+        for(int i=0; i<s.length(); i++){
+            char chS = s.charAt(i);
+            char chT = t.charAt(i);
+            if(mapS[chS] == 0 && mapT[chT] == 0){
+                mapS[chS] = chT;
+                mapT[chT] = chS;
+            } else if(mapS[chS] != chT || mapT[chT] != chS){
+                return false;
+            }
+        }
+        return true;
+    }
+    // longest common prefix - leetcode 14
+    public static String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0) return "";
+        
+    }
+}
---
+++ Strings/Medium/Main1.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println(sumSubstrings("6759"));
+    }
+    // sum of beauty of all substrings - leetcode 1781
+    public static int beautySum(String s) {
+        int sum=0;
+        int n=s.length();
+        for (int i = 0; i < n; i++) {
+            int[] freq = new int[26];
+            for (int j = i; j < n; j++) {
+                char ch = s.charAt(j);
+                freq[ch - 'a']++;
+                int maxFreq = 0;
+                int minFreq = Integer.MAX_VALUE;
+                for (int k = 0; k < 26; k++) {
+                    if (freq[k] > 0) {
+                        maxFreq = Math.max(maxFreq, freq[k]);
+                        minFreq = Math.min(minFreq, freq[k]);
+                    }
+                }
+                sum += (maxFreq - minFreq);
+            }
+        }
+        return sum;
+    }
+    // Sum of all substrings of a number- GFG
+    public static int sumSubstrings(String s) {
+        // code here
+        int n = s.length();
+        int totalSum = 0;
+        int prev = 0; 
+        for (int i = 0; i < n; i++) {
+            int digit = s.charAt(i) - '0';
+            int curr = prev * 10 + (i + 1) * digit;
+            totalSum += curr;
+            prev = curr;
+        }
+
+        return totalSum;
+    }
+
+}
