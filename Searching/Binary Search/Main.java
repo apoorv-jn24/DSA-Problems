3002e5f50b53ca46e8a00b38e160f8cf63bba131
Apoorv Jain <apoorvjainji@gmail.com>
2026-02-08 21:25:32 +0530

    single element

--- Searching/Binary Search/Main.java
+++ Searching/Binary Search/Main.java
@@ -124,4 +124,23 @@ public class Main {
         }
         return nums[low];
     }
+    // single element in a sorted array- leetcode 540
+    public static int singleNonDuplicate(int[] nums) {
+        int start=0, end=nums.length-1;
+        while(start<=end){
+            int mid= start + (end-start)/2;
+            if( (mid==0 || nums[mid-1]!= nums[mid] )&& (mid==nums.length-1 || nums[mid]!= nums[mid+1])) 
+             return nums[mid];
+            else if(mid>0 && nums[mid-1]==nums[mid]){
+                int length = mid-start-1;
+                if(length%2==0) start=mid+1;
+                else end=mid-2;
+            }else{
+                int length = end-mid-1;
+                if(length%2==0) end=mid-1;
+                else start=mid+2;
+            }
+        }
+        return -1;
+    }
 }
