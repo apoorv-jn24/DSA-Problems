0d2fdbb9a65ee6c59d65c848502541dcd1d299b7
Apoorv Jain <apoorvjainji@gmail.com>
2026-02-14 10:38:44 +0530

    Bit Manipulation

--- Bit Manipulation/Main1.java
+++ Bit Manipulation/Main1.java
@@ -20,4 +20,13 @@ public class Main1 {
     public int singleNumber2(int[] nums) {
         
     }
+    // Minimum Bit Flips to Convert Number
+     public static int minBitFlips(int start, int goal) {
+        int ans= (start^goal);
+        int count=0;
+        for(int i=0; i<31; i++){
+            if((ans & (1<<i)) !=0) count++;
+        }
+        return count;
+    }
 }
