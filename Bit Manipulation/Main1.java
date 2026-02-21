0ba52c99471d25aee81cdaf93b5ceb72ed3217b5
Apoorv Jain <apoorvjainji@gmail.com>
2026-02-21 18:15:03 +0530

    Updates

--- Bit Manipulation/Main1.java
+++ Bit Manipulation/Main1.java
@@ -123,5 +123,20 @@ public class Main1 {
         return ans;
 
     }
-
+    // LeetCode - 762
+    public int countPrimeSetBits(int left, int right) {
+        int count=0;
+        for (int i=left;i<=right;i++) {
+            int setBits=Integer.bitCount(i);
+            if (isPrime(setBits)) {
+                count++;
+            }
+        }
+        return count;
+    }
+      private boolean isPrime(int n) {
+        if(n==2 || n==3 || n==5 || n==7 || n==11 || n==13 || n==17
+         || n==19 || n==23 || n==31) return true;
+        return false;
+    }
 }
